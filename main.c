4055db1097a25c6e03a61931a2a1eb455d15c491
Mondrol <dieaconurobert13@gmail.com>
2026-01-29 22:14:39 +0200

    commit facut de patricia, cu niste modificari in main

--- main.c
+++ main.c
@@ -2,5 +2,7 @@
 
 int main() {
     printf("ceva de afisata aici\n");
+    //aceasta zona din cod a fost adaugata de patricia
+    printf ("cred ca am prins spilul la treaba asta cu git\n");
     return 0;
 }
