6fb9e83c5947a9d4b06534ca40bf9f637874a0da
Mondrol <dieaconurobert13@gmail.com>
2026-01-29 22:21:04 +0200

    un ultim commit de la robert

--- main.c
+++ main.c
@@ -4,5 +4,7 @@ int main() {
     printf("ceva de afisata aici\n");
     //aceasta zona din cod a fost adaugata de patricia
     printf ("cred ca am prins spilul la treaba asta cu git\n");
+    //a mai adaugat si robert ceva
+    printf ("un ultim printf pe seara asta\n");
     return 0;
 }
